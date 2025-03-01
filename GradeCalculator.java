0ea8bb20512fd549d3d54cd3ef91fd442c403102
Immie-Lubay <lubayimmierose_bscs@plmun.edu.ph>
2025-03-01 09:16:30 +0800

    Submitting OBJPROG Week 05 - Laboratory # 17

---
+++ GradeCalculator.java
@@ -0,0 +1,29 @@
+public class GradeCalculator {
+    public static void main(String[] args) {
+        System.out.println("Enter your score: ");
+        System.out.println("Your grade is: ");
+        float getStudentScore;
+    }
+    public static void getStudentScore()
+        
+        if (getStudentScore >= 60) {
+            if (getStudentScoree >= 90) {
+               System.out.println("Grade: A");
+            } else {
+               if (getStudentScore >= 80) {
+                     System.out.println("Grade: B");
+               } else {
+                    if (getStudentScore >= 70) {
+                        System.out.println("Grade: C");
+                    }
+                    } else {
+                        if (getStudentScore >= 60) {
+                            System.out.println("Grade: E");
+                        }
+                    }    
+            }
+         } else {
+            System.out.println("Grade: F");
+         }
+         input.close();
+}
